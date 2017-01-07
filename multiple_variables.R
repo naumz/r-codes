4998efee62e54302045e7591b0951515e1450dd0
Naumaan N <naumz@users.noreply.github.com>
2017-01-06 16:21:24 -0800

    Added additional outlier detection methods

--- multiple_variables.R
+++ multiple_variables.R
@@ -9,7 +9,13 @@ rm(list=ls())
 
 # Import required libraries
 library(MASS)
-library(car)
+library(car)  # for outliers
+library(outliers)  # for outliers
+library(mvoutlier)  # for outliers
+library(DMwR)  # for outliers
+library(mclust)  # for outliers
+library(cluster)  # for outliers
+library(fpc)  # for outliers
 
 # import data from csv
 mydata <- read.csv('Datasets/mlr06.csv')
@@ -23,8 +29,16 @@ model1 <- lm(fmla, data=mydata)
 print(summary(model1))
 
 # identify outliers (12, 38, 41, 48)
-print(outlierTest(model1))
-plot(model1,which=c(2,5))
+print(outlierTest(model1))  # method 1 - Bonferroni p-value method
+plot(model1,which=c(2,5))  # method 2 - QQ, Residual, Cook
+chisq.plot(mydata)  # method 3 - Robust Mahalanobis Distance
+outlier <- sort(lofactor(mydata,25),index.return=TRUE)
+b <- outlier$ix
+outliers.LOF <- tail(b,3)  # method 4 - 3 outliers using Local Outlier Factor (k=25)
+fit <-kmeans(mydata,4)
+s<- sort(fit$size,index.return=TRUE)
+clusplot(mydata, fit$cluster, cex=1, col.p=fit$cluster)
+outliers.clu <- which(fit$cluster==s$ix[1])  # method 5 - k-means clustering (k=4)
 
 # center independent variables about zero (fixes insignificant intercept issue etc.). Alternative to eval-parse code below is to use eval(as.name()) or get() with column numbers.
 mydata.centered = data.frame(X1=mydata$X1)
